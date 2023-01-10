796171bfba5be58297e2f99166a9993cd47f4959
Zharkipas <zharbiter617@gmail.com>
2023-01-10 14:29:30 +0800

    update

--- img cap.py
+++ img cap.py
@@ -2,28 +2,29 @@ import cv2
 import os
 import keyboard
 import csv
-import time
 
 path = r'C:\Users\Zharb\Documents\GitHub\Image-capture\IMG'
 cam = cv2.VideoCapture(0)
 img_counter = 0
 video = 'video'
+angle = 0
 #t_end = time.time() + 60 * 0.5
 #while time.time() < t_end:
 while True:
 
     result, image = cam.read()
-    Cat = ['Image', 'Angle']
+    Cat = ['center','left','right', 'steering', 'throttle', 'reverse', 'speed']
 
     if result:
         cv2.imshow(video, image)
         if keyboard.is_pressed('c'):
             print("c is pressed")
-            img_name=f'frame_{img_counter}.png'
+            img_name=f'center_{img_counter}.png'
             file = os.path.join(path, img_name)
             cv2.imwrite(file, image)
             img_counter += 1
-            dict ={"Image": file, "Angle":"0"}
+            #angle=current_angle
+            dict ={"center": file,"left":'null',"right":'null', "steering": angle, "throttle":'null', "reverse":'null', "speed":'null'}
             with open('demo.csv', 'a') as csv_file:
                 dict_object = csv.DictWriter(csv_file, fieldnames = Cat) 
                 dict_object.writerow(dict)
